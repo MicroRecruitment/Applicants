8e423674207d1e08406e4fc8bf26bf42b1622bac
Linus Berg <linusberg@localhost.localdomain>
2020-01-22 17:17:50 +0100

    Intro stuff

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+const express = require('express');
+const io = require('socket.io');
+const http = require('http');
+
+
+var app = express();
+var srv = http.createServer(app);
+var socket = io(srv);
+const PORT = 3030;
+
+app.use(express.static('./public'));
+
+srv.listen(PORT, function(){
+  console.log('Server started on *:' + PORT);
+});

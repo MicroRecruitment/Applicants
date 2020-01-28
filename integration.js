5e92343ded50ddf25e31002c238e222c843bc221
Linus Berg <linusberg@n147-p228.eduroam.kth.se>
2020-01-28 15:31:56 +0100

    Register stuff.

---
+++ integration.js
@@ -0,0 +1,46 @@
+const db = require('ibm_db');
+const URL = 'DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-lon02-01.services.eu-gb.bluemix.net;PORT=50000;PROTOCOL=TCPIP;UID=vrj51280;PWD=kd7l76c81-81v28q;';
+
+const INSERT = 'INSERT INTO person (name, surname, username, password, role_id) VALUES (?, ?, ?, ?, ?, ?)';
+const ROLE = 'INSERT INTO role (role_id, name) VALUES (?, ?)';
+
+class Database {
+  constructor() {
+    this.connection_ = db.openSync(URL);
+  }
+
+  Connect(err, conn) {
+    if (err) {
+      return console.log(err);
+    }
+    this.connection_ = conn;
+  }
+
+  AddUser(data, cb) {
+    var binds = [
+      data.person_id,
+      data.name,
+      data.surname,
+      data.username,
+      data.password,
+      data.role_id
+    ];
+    this.connection_.query(INSERT, binds, function (err, rows, sqlca) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(rows);
+      }
+    });
+  }
+}
+
+var test = new Database();
+var dat = {
+  name: 'Linus',
+  surname: 'Berg',
+  password: 'lol',
+  username: 'big',
+  role_id: 1
+}
+//test.AddUser(dat, null);

a05647ce9d19d1ac96d059f55b69556f377b900f
Marcus Ingelborn <mringelborn@gmail.com>
2020-02-26 12:25:28 +0100

    Removed socket.io

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 'use strict';
 const express = require('express');
-const io = require('socket.io');
 const http = require('http');
 
 const ctrl = require('./controller.js');
 
 var app = express();
 var srv = http.createServer(app);
-var socket = io(srv);
 const PORT = Math.round(Math.random() * 40000);
 
 app.use(express.static('./public'));

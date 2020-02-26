89aa163d82fdb951dea8b4602ed75a910a25ddca
Linus Berg <linusberg@n139-p161.eduroam.kth.se>
2020-02-26 13:20:20 +0100

    Javadoc

--- model.js
+++ model.js
@@ -17,25 +17,57 @@ class Model {
     }
     return ret;
   }
+  /*
+	* Get All Competences.
+  * @queue: auth_queue
+	* @author: Linus Berg
+	* @return {obj} Queue Reply Status.
+	*/
   async GetCompetences() {
     let result = await db.GetCompetences();
     return await this.ProcessResult(result);
   }
+  /*
+	* Get All Users.
+  * @queue: auth_queue
+	* @author: Linus Berg
+	* @return {obj} Queue Reply Status.
+	*/
   async GetAllUsers() {
     let result = await db.GetAllUsers();
     return await this.ProcessResult(result);
   }
 
+  /*
+	* Set Application State.
+  * @queue: auth_queue
+	* @author: Linus Berg
+	* @param {obj} username and status.
+	* @return {obj} Queue Reply Status.
+	*/
   async SetApplicant(applicant_data) {
     let result = await db.SetApplicant(applicant_data);
     return await this.ProcessResult(result);
   }
 
+  /*
+	* Send Application to db.
+  * @queue: auth_queue
+	* @author: Linus Berg
+	* @param {obj} application data.
+	* @return {obj} Queue Reply Status.
+	*/
   async Apply(application_data) {
     let result = await db.Apply(application_data);
     return await this.ProcessResult(result);
   }
 
+  /*
+	* Get All Applicants.
+  * @queue: auth_queue
+	* @author: Linus Berg
+	* @return {obj} Queue Reply Status.
+	*/
   async GetAllApplicants() {
     let result = await db.GetAllApplicants();
     console.log(result);

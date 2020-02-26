fb807691b24fef24571440d0be6109e6d2e880ed
Marcus Ingelborn <mringelborn@gmail.com>
2020-02-26 12:02:59 +0100

    Added copetences to applicants list

--- sql/get_all_apps.sql
+++ sql/get_all_apps.sql
@@ -13,11 +13,26 @@ SELECT
             ', '
         ) ||
         '}'
-    ) as availabilities
+    ) as availabilities,
+    (
+        '{' ||
+        LISTAGG(
+            '"' || competence.competence_id || '" : {' ||
+            '"name" : "' || competence.name || '", ' ||
+            '"years_of_experience" : "' || competence_profile.years_of_experience || '"' ||
+            '}',
+            ', '
+        ) ||
+        '}'
+    ) as competences
 FROM
     person
 LEFT JOIN
     availability ON availability.person_id = person.person_id
+LEFT JOIN
+    competence_profile ON competence_profile.person_id = person.person_id
+LEFT JOIN
+    competence ON competence.competence_id = competence_profile.competence_id
 WHERE
     person.status = 1
 GROUP BY

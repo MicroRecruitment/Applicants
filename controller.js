d5a547c0d3d773d45b15b95c83aaaa896eadbb66
Linus Berg <linusberg@n165-p162.eduroam.kth.se>
2020-03-02 10:51:58 +0100

    Formatting

--- controller.js
+++ controller.js
@@ -25,7 +25,7 @@ class Controller {
        * frame.username - Username of user to set
        */
       SetApplicant: async function(frame) {
-        console.log("Controller: SetApplicant");
+        console.log('Controller: SetApplicant');
         console.log(frame);
         return that.logic_.SetApplicant(frame.content);
       },
--- integration.js
+++ integration.js
@@ -3,13 +3,17 @@ const URL =
   'DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-lon02-01.services.eu-gb.bluemix.net;PORT=50000;PROTOCOL=TCPIP;UID=vrj51280;PWD=kd7l76c81-81v28q;';
 
 const GET_ALL_USERS = 'SELECT name, surname, email FROM person';
-const GET_ALL_APPS = require('fs').readFileSync('./sql/get_all_apps.sql').toString();
+const GET_ALL_APPS = require('fs')
+  .readFileSync('./sql/get_all_apps.sql')
+  .toString();
 
 const GET_COMPETENCES = 'SELECT competence_id, name FROM competence';
 const SET_APPLICANT = 'UPDATE person SET status = ? WHERE person_id = ?';
 
-const ADD_AVAILABILITY = 'INSERT INTO availability (person_id, from_date, to_date) VALUES(?, ?, ?)';
-const ADD_COMPETENCE = 'INSERT INTO competence_profile (person_id, competence_id, years_of_experience) VALUES(?, ?, ?)';
+const ADD_AVAILABILITY =
+  'INSERT INTO availability (person_id, from_date, to_date) VALUES(?, ?, ?)';
+const ADD_COMPETENCE =
+  'INSERT INTO competence_profile (person_id, competence_id, years_of_experience) VALUES(?, ?, ?)';
 
 class Database {
   constructor() {
@@ -35,28 +39,20 @@ class Database {
     const user = application_data.id;
     for (let i = 0; i < application_data.avail.length; ++i) {
       const availability = application_data.avail[i];
-      const binds = [
-        user,
-        availability.from,
-        availability.to
-      ];
+      const binds = [user, availability.from, availability.to];
       console.log('AVAIL BINDS', binds);
       let res = this.connection_.querySync(ADD_AVAILABILITY, binds);
-      console.log(res); 
-    }
-    
-    for (let i = 0; i < application_data.comp.length; ++i) {
-      const competence = application_data.comp[i];
-      const binds = [
-        user,
-        competence.id,
-        competence.years,
-      ];
-      let res = this.connection_.querySync(ADD_COMPETENCE, binds);
-      console.log(res); 
+      console.log(res);
     }
 
-    this.SetApplicant({status: 1, id: user});
+    for (let i = 0; i < application_data.comp.length; ++i) {
+      const competence = application_data.comp[i];
+      const binds = [user, competence.id, competence.years];
+      let res = this.connection_.querySync(ADD_COMPETENCE, binds);
+      console.log(res);
+    }
+
+    this.SetApplicant({ status: 1, id: user });
     return [];
   }
 
